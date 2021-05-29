dca89b4eb4f6d2d256af2a2701c3569bce2b1008
ghaneshmouthouvel <ghaneshmouthouvel@gmail.com>
2021-05-29 10:39:08 +0530

    added models

--- coaid/home/data.py
+++ coaid/home/data.py
@@ -16,21 +16,39 @@ def Get_Data():
     mydict["date"]=data["parameters"]["day"]
     mydict["active"]=data["response"][0]["cases"]["active"]
     mydict["critical"]=data["response"][0]["cases"]["critical"]
-    mydict["new"]=data["response"][0]["cases"]["new"][1:]
+    mydict["apd"]=data["response"][0]["cases"]["new"][1:]
     mydict["recovered"]=data["response"][0]["cases"]["recovered"]
     mydict["total_case"]=data["response"][0]["cases"]["total"]
+    mydict["dpd"]=data["response"][0]["deaths"]["new"]
     mydict["total_death"]=data["response"][0]["deaths"]["total"]
+    mydict["tests"]=data["response"][0]['tests']['total']
     return mydict
 
 def Post_Data():
     mydict = Get_Data()
-
+    # Update info
+    Info = updateinfo()
+    Info.timesnap = mydict['time']
+    Info.save()
+    # Active Cases
     Active = Active_cases()
     Active.date = datetime.strptime(mydict['date'], "%Y-%m-%d")
     Active.active = mydict['active']
     Active.critical = mydict['critical']
-    Active.new = mydict['new']
+    Active.new = mydict['apd']
     Active.recovered = mydict['recovered']
     Active.total = mydict['total_case']
     Active.save()
+    # Death Cases
+    Deaths = Death_cases()
+    Deaths.date = datetime.strptime(mydict['date'], "%Y-%m-%d")
+    Deaths.new = mydict['dpd']
+    Deaths.total = mydict['total_death']
+    Deaths.save()
+    # Test Made
+    Test = Covid_tests()
+    Test.date = datetime.strptime(mydict['date'], "%Y-%m-%d")
+    Test.total = mydict['tests']
+    Test.save()
+
 
