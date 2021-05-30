672bf1b49021a0869688a7742d1bcffd96cf0c10
ghaneshmouthouvel <ghaneshmouthouvel@gmail.com>
2021-05-30 08:58:08 +0530

    vaccination page

--- coaid/vaccinationpage/views.py
+++ coaid/vaccinationpage/views.py
@@ -5,14 +5,6 @@ from datetime import datetime, timedelta
 
 # Create your views here.
 def vacpage(request):
-    if request.method == 'POST':
-        form = SearchByPinForm(request.POST)
-        if form.is_valid():
-            pincode = request.POST['pincode']
-            age = request.POST['age']
-    else:
-        form = SearchByPinForm()
-        
     def vaccine_available(Age,Pincode,Date):
         if Age>44:
             set_age = 45
@@ -48,31 +40,19 @@ def vacpage(request):
                         session["status"]=accept
                     session["hos_name"]= hos["name"]
                     availabe_vaccine.append(session)
-                
-
         return availabe_vaccine
 
-    date = datetime.today().strftime("%d-%m-%Y")
-    vaccine = vaccine_available(int(age),pincode,date)
-    
-    hospitalname = []
-    for hos in vaccine:
-        hospitalname.append(hos['hos_name'])
-    rangelist = range(len(hospitalname))
 
-    accept = '''<a class="bg-green-500 text-white px-4 py-2 border rounded-md hover:bg-white hover:border-green-500 hover:text-black " href="https://selfregistor.cowin.gov.in">
-                              Register
-                            </a>'''
-    booked = '''<button class="bg-red-500 text-white px-4 py-2 border rounded-md">
-                              Booked
-                            </button>'''
-    
-
-    date1 = datetime.today()
-    date2 = date1 + timedelta(1)
-    date3 = date2 + timedelta(1)
-    date4 = date3 + timedelta(1)
-    date5 = date4 + timedelta(1)
-    
-    context = {'form':form, 'date1':date1.strftime('%b %d,%Y'), 'date2':date2.strftime('%b %d,%Y'), 'date3':date3.strftime('%b %d,%Y'), 'date4':date4.strftime('%b %d,%Y'), 'date5':date5.strftime('%b %d,%Y'), 'name':vaccine, 'rangelist':rangelist }
+    if request.method == 'POST':
+        form = SearchByPinForm(request.POST)
+        if form.is_valid():
+            pincode = request.POST['pincode']
+            age = request.POST['age']
+            date = datetime.today().strftime("%d-%m-%Y")
+            vaccine = vaccine_available(int(age),pincode,date)
+        context = {'form':form, 'name':vaccine}
+    else:
+        form = SearchByPinForm()
+        context = {'form':form}
+        
     return render(request, 'vacpage/index.html', context)
