ea8707e5bee829fe66be7d4c0923dcf5d317d612
ghaneshmouthouvel <ghaneshmouthouvel@gmail.com>
2021-05-30 09:37:25 +0530

    chatbot

--- coaid/coaid/urls.py
+++ coaid/coaid/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
     path('FindCovisheild/', include('vaccinationpage.urls')),
+    path('chatbot/',include('chatbot.urls')),
 ]
